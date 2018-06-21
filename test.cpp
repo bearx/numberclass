9d4678355e2f35d7db69b4484ea7f677f3faeba5
bearx <815018345@qq.com>
2018-06-21 16:34:08 +0800

    666

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include"number.h"
+int main()
+{
+	Number* p;
+	RealNumber a(14.8),b(15.1);
+	RealNumber c=a.add(b);
+	p=&c;
+	(*p).print();
+	IntegerNumber a1(14),b1(15);
+	IntegerNumber c1=a1.add(b1);
+	p=&c1;
+	(*p).print();
+	ComplexNumber a2(14),b2(15);
+	ComplexNumber c2=a2.add(b2);
+	p=&c2;
+	(*p).print();
+	return 0;
+}
