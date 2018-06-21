5eb682c3e44c510a6c26d618b38ea1759e6e5a93
bearx <815018345@qq.com>
2018-06-21 16:42:30 +0800

    new ver

--- test.cpp
+++ test.cpp
@@ -10,7 +10,7 @@ int main()
 	IntegerNumber c1=a1.add(b1);
 	p=&c1;
 	(*p).print();
-	ComplexNumber a2(14),b2(15);
+	ComplexNumber a2(complex<double>(14,12)),b2(15);
 	ComplexNumber c2=a2.add(b2);
 	p=&c2;
 	(*p).print();
